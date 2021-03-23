e511f1fd2bf394f5a9965dc3b26e2480b48ebb42
jdmedlock <jdmedlock@gmail.com>
2021-03-23 14:57:10 -0500

    Feature: Update photos and landing page text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -23,7 +23,7 @@
         <div class="w-full lg:w-10/12 px-4 ml-auto mr-auto text-center">
           <div class="pr-12">
             <h1 class="text-white font-semibold text-5xl">
-              Your story started here
+              Your story started here!
             </h1>
             <p class="mt-4 text-lg text-gray-300">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -65,7 +65,7 @@
                 w-full mb-6 shadow-lg rounded-lg bg-sepia-300">
                 <img
                   alt="..."
-                  src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1051&amp;q=80"
+                  src="chs_friends.png"
                   class="w-full align-middle rounded-t-lg" />
                 <blockquote class="relative p-8 mb-4">
                   <svg
@@ -99,7 +99,7 @@
                 w-full mb-6 shadow-lg rounded-lg bg-sepia-300">
                 <img
                   alt="..."
-                  src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1051&amp;q=80"
+                  src="chs_graduation.png"
                   class="w-full align-middle rounded-t-lg" />
                 <blockquote class="relative p-8 mb-4">
                   <svg
@@ -132,7 +132,7 @@
                 w-full mb-6 shadow-lg rounded-lg bg-sepia-300">
                 <img
                   alt="..."
-                  src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1051&amp;q=80"
+                  src="chs_basketball.jpeg"
                   class="w-full align-middle rounded-t-lg" />
                 <blockquote class="relative p-8 mb-4">
                   <svg
@@ -185,7 +185,7 @@
           <img
             alt="..."
             class="max-w-full rounded-lg shadow-lg"
-            src="https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=634&amp;q=80" />
+            src="" />
         </div>
         <div class="w-full md:w-5/12 ml-auto mr-auto px-4">
           <div class="md:pr-12">
@@ -194,217 +194,18 @@
               justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-pink-300">
               <i class="fas fa-rocket text-xl" />
             </div>
-            <h3 class="text-3xl font-semibold">A growing company</h3>
+            <h3 class="text-3xl font-semibold">More information is on the way...</h3>
             <p class="mt-4 text-lg leading-relaxed text-gray-600">
-              The extension comes with three pre-built pages to help you get
-              started faster. You can change the text and images and you're good
-              to go.
+              Check back frequently to get the latest news and information 
+							about your reunion!
             </p>
-            <ul class="list-none mt-6">
-              <li class="py-2">
-                <div class="flex items-center">
-                  <div>
-                    <span
-                      class="text-xs font-semibold inline-block py-1 px-2
-                      uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
-                      <i class="fas fa-fingerprint" />
-                    </span>
-                  </div>
-                  <div>
-                    <h4 class="text-gray-600">Carefully crafted components</h4>
-                  </div>
-                </div>
-              </li>
-              <li class="py-2">
-                <div class="flex items-center">
-                  <div>
-                    <span
-                      class="text-xs font-semibold inline-block py-1 px-2
-                      uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
-                      <i class="fab fa-html5" />
-                    </span>
-                  </div>
-                  <div>
-                    <h4 class="text-gray-600">Amazing page examples</h4>
-                  </div>
-                </div>
-              </li>
-              <li class="py-2">
-                <div class="flex items-center">
-                  <div>
-                    <span
-                      class="text-xs font-semibold inline-block py-1 px-2
-                      uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
-                      <i class="far fa-paper-plane" />
-                    </span>
-                  </div>
-                  <div>
-                    <h4 class="text-gray-600">Dynamic components</h4>
-                  </div>
-                </div>
-              </li>
-            </ul>
+          
           </div>
         </div>
       </div>
     </div>
   </section>
-  <section class="pt-20 pb-48">
-    <div class="container mx-auto px-4">
-      <div class="flex flex-wrap justify-center text-center mb-24">
-        <div class="w-full lg:w-6/12 px-4">
-          <h2 class="text-4xl font-semibold">Here are our heroes</h2>
-          <p class="text-lg leading-relaxed m-4 text-gray-600">
-            According to the National Oceanic and Atmospheric Administration,
-            Ted, Scambos, NSIDClead scentist, puts the potentially record
-            maximum.
-          </p>
-        </div>
-      </div>
-      <div class="flex flex-wrap">
-        <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
-          <div class="px-6">
-            <img
-              alt="..."
-              src="./assets/img/team-1-800x800.jpg"
-              class="shadow-lg rounded-full max-w-full mx-auto"
-              style="max-width: 120px;" />
-            <div class="pt-6 text-center">
-              <h5 class="text-xl font-bold">Ryan Tompson</h5>
-              <p class="mt-1 text-sm text-gray-500 uppercase font-semibold">
-                Web Developer
-              </p>
-              <div class="mt-6">
-                <button
-                  class="bg-blue-400 text-white w-8 h-8 rounded-full
-                  outline-none focus:outline-none mr-1 mb-1"
-                  type="button">
-                  <i class="fab fa-twitter" />
-                </button>
-                <button
-                  class="bg-blue-600 text-white w-8 h-8 rounded-full
-                  outline-none focus:outline-none mr-1 mb-1"
-                  type="button">
-                  <i class="fab fa-facebook-f" />
-                </button>
-                <button
-                  class="bg-pink-500 text-white w-8 h-8 rounded-full
-                  outline-none focus:outline-none mr-1 mb-1"
-                  type="button">
-                  <i class="fab fa-dribbble" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
-          <div class="px-6">
-            <img
-              alt="..."
-              src="./assets/img/team-2-800x800.jpg"
-              class="shadow-lg rounded-full max-w-full mx-auto"
-              style="max-width: 120px;" />
-            <div class="pt-6 text-center">
-              <h5 class="text-xl font-bold">Romina Hadid</h5>
-              <p class="mt-1 text-sm text-gray-500 uppercase font-semibold">
-                Marketing Specialist
-              </p>
-              <div class="mt-6">
-                <button
-                  class="bg-red-600 text-white w-8 h-8 rounded-full outline-none
-                  focus:outline-none mr-1 mb-1"
-                  type="button">
-                  <i class="fab fa-google" />
-                </button>
-                <button
-                  class="bg-blue-600 text-white w-8 h-8 rounded-full
-                  outline-none focus:outline-none mr-1 mb-1"
-                  type="button">
-                  <i class="fab fa-facebook-f" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
-          <div class="px-6">
-            <img
-              alt="..."
-              src="./assets/img/team-3-800x800.jpg"
-              class="shadow-lg rounded-full max-w-full mx-auto"
-              style="max-width: 120px;" />
-            <div class="pt-6 text-center">
-              <h5 class="text-xl font-bold">Alexa Smith</h5>
-              <p class="mt-1 text-sm text-gray-500 uppercase font-semibold">
-                UI/UX Designer
-              </p>
-              <div class="mt-6">
-                <button
-                  class="bg-red-600 text-white w-8 h-8 rounded-full outline-none
-                  focus:outline-none mr-1 mb-1"
-                  type="button">
-                  <i class="fab fa-google" />
-                </button>
-                <button
-                  class="bg-blue-400 text-white w-8 h-8 rounded-full
-                  outline-none focus:outline-none mr-1 mb-1"
-                  type="button">
-                  <i class="fab fa-twitter" />
-                </button>
-                <button
-                  class="bg-gray-800 text-white w-8 h-8 rounded-full
-                  outline-none focus:outline-none mr-1 mb-1"
-                  type="button">
-                  <i class="fab fa-instagram" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="w-full md:w-6/12 lg:w-3/12 lg:mb-0 mb-12 px-4">
-          <div class="px-6">
-            <img
-              alt="..."
-              src="./assets/img/team-4-470x470.png"
-              class="shadow-lg rounded-full max-w-full mx-auto"
-              style="max-width: 120px;" />
-            <div class="pt-6 text-center">
-              <h5 class="text-xl font-bold">Jenna Kardi</h5>
-              <p class="mt-1 text-sm text-gray-500 uppercase font-semibold">
-                Founder and CEO
-              </p>
-              <div class="mt-6">
-                <button
-                  class="bg-pink-500 text-white w-8 h-8 rounded-full
-                  outline-none focus:outline-none mr-1 mb-1"
-                  type="button">
-                  <i class="fab fa-dribbble" />
-                </button>
-                <button
-                  class="bg-red-600 text-white w-8 h-8 rounded-full outline-none
-                  focus:outline-none mr-1 mb-1"
-                  type="button">
-                  <i class="fab fa-google" />
-                </button>
-                <button
-                  class="bg-blue-400 text-white w-8 h-8 rounded-full
-                  outline-none focus:outline-none mr-1 mb-1"
-                  type="button">
-                  <i class="fab fa-twitter" />
-                </button>
-                <button
-                  class="bg-gray-800 text-white w-8 h-8 rounded-full
-                  outline-none focus:outline-none mr-1 mb-1"
-                  type="button">
-                  <i class="fab fa-instagram" />
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
+
   <section class="pb-20 relative block bg-gray-900">
     <div
       class="bottom-auto top-0 left-0 right-0 w-full absolute
@@ -426,53 +227,8 @@
     <div class="container mx-auto px-4 lg:pt-24 lg:pb-64">
       <div class="flex flex-wrap text-center justify-center">
         <div class="w-full lg:w-6/12 px-4">
-          <h2 class="text-4xl font-semibold text-white">Build something</h2>
+          <h2 class="text-4xl font-semibold text-white">Get involved!</h2>
           <p class="text-lg leading-relaxed mt-4 mb-4 text-gray-500">
-            Put the potentially record low maximum sea ice extent tihs year down
-            to low ice. According to the National Oceanic and Atmospheric
-            Administration, Ted, Scambos.
-          </p>
-        </div>
-      </div>
-      <div class="flex flex-wrap mt-12 justify-center">
-        <div class="w-full lg:w-3/12 px-4 text-center">
-          <div
-            class="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white
-            inline-flex items-center justify-center">
-            <i class="fas fa-medal text-xl" />
-          </div>
-          <h6 class="text-xl mt-5 font-semibold text-white">
-            Excelent Services
-          </h6>
-          <p class="mt-2 mb-4 text-gray-500">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-        </div>
-        <div class="w-full lg:w-3/12 px-4 text-center">
-          <div
-            class="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white
-            inline-flex items-center justify-center">
-            <i class="fas fa-poll text-xl" />
-          </div>
-          <h5 class="text-xl mt-5 font-semibold text-white">
-            Grow your market
-          </h5>
-          <p class="mt-2 mb-4 text-gray-500">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-        </div>
-        <div class="w-full lg:w-3/12 px-4 text-center">
-          <div
-            class="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white
-            inline-flex items-center justify-center">
-            <i class="fas fa-lightbulb text-xl" />
-          </div>
-          <h5 class="text-xl mt-5 font-semibold text-white">Launch time</h5>
-          <p class="mt-2 mb-4 text-gray-500">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
           </p>
         </div>
       </div>
@@ -486,9 +242,10 @@
             class="relative flex flex-col min-w-0 break-words w-full mb-6
             shadow-lg rounded-lg bg-gray-300">
             <div class="flex-auto p-5 lg:p-10">
-              <h4 class="text-2xl font-semibold">Want to work with us?</h4>
+              <h4 class="text-2xl font-semibold">We want to hear from you!</h4>
               <p class="leading-relaxed mt-1 mb-4 text-gray-600">
-                Complete this form and we will get back to you in 24 hours.
+                Complete this form and Dale Hahs, Mayor McCheese of the
+								CHS '73 Organizing Committee, will get back to you in 24 hours.
               </p>
               <div class="relative w-full mb-3 mt-8">
                 <label
@@ -569,139 +326,11 @@
     </svg>
   </div>
   <div class="container mx-auto px-4">
-    <div class="flex flex-wrap">
-      <div class="w-full lg:w-6/12 px-4">
-        <h4 class="text-3xl font-semibold">Let's keep in touch!</h4>
-        <h5 class="text-lg mt-0 mb-2 text-gray-700">
-          Find us on any of these platforms, we respond 1-2 business days.
-        </h5>
-        <div class="mt-6">
-          <button
-            class="bg-white text-blue-400 shadow-lg font-normal h-10 w-10
-            items-center justify-center align-center rounded-full outline-none
-            focus:outline-none mr-2 p-3"
-            type="button">
-            <i class="flex fab fa-twitter" />
-          </button>
-          <button
-            class="bg-white text-blue-600 shadow-lg font-normal h-10 w-10
-            items-center justify-center align-center rounded-full outline-none
-            focus:outline-none mr-2 p-3"
-            type="button">
-            <i class="flex fab fa-facebook-square" />
-          </button>
-          <button
-            class="bg-white text-pink-400 shadow-lg font-normal h-10 w-10
-            items-center justify-center align-center rounded-full outline-none
-            focus:outline-none mr-2 p-3"
-            type="button">
-            <i class="flex fab fa-dribbble" />
-          </button>
-          <button
-            class="bg-white text-gray-900 shadow-lg font-normal h-10 w-10
-            items-center justify-center align-center rounded-full outline-none
-            focus:outline-none mr-2 p-3"
-            type="button">
-            <i class="flex fab fa-github" />
-          </button>
-        </div>
-      </div>
-      <div class="w-full lg:w-6/12 px-4">
-        <div class="flex flex-wrap items-top mb-6">
-          <div class="w-full lg:w-4/12 px-4 ml-auto">
-            <span
-              class="block uppercase text-gray-600 text-sm font-semibold mb-2">
-              Useful Links
-            </span>
-            <ul class="list-unstyled">
-              <li>
-                <a
-                  class="text-gray-700 hover:text-gray-900 font-semibold block
-                  pb-2 text-sm"
-                  href="https://www.creative-tim.com/presentation">
-                  About Us
-                </a>
-              </li>
-              <li>
-                <a
-                  class="text-gray-700 hover:text-gray-900 font-semibold block
-                  pb-2 text-sm"
-                  href="https://blog.creative-tim.com">
-                  Blog
-                </a>
-              </li>
-              <li>
-                <a
-                  class="text-gray-700 hover:text-gray-900 font-semibold block
-                  pb-2 text-sm"
-                  href="https://www.github.com/creativetimofficial">
-                  Github
-                </a>
-              </li>
-              <li>
-                <a
-                  class="text-gray-700 hover:text-gray-900 font-semibold block
-                  pb-2 text-sm"
-                  href="https://www.creative-tim.com/bootstrap-themes/free">
-                  Free Products
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="w-full lg:w-4/12 px-4">
-            <span
-              class="block uppercase text-gray-600 text-sm font-semibold mb-2">
-              Other Resources
-            </span>
-            <ul class="list-unstyled">
-              <li>
-                <a
-                  class="text-gray-700 hover:text-gray-900 font-semibold block
-                  pb-2 text-sm"
-                  href="https://github.com/creativetimofficial/argon-design-system/blob/master/LICENSE.md">
-                  MIT License
-                </a>
-              </li>
-              <li>
-                <a
-                  class="text-gray-700 hover:text-gray-900 font-semibold block
-                  pb-2 text-sm"
-                  href="https://creative-tim.com/terms">
-                  Terms &amp; Conditions
-                </a>
-              </li>
-              <li>
-                <a
-                  class="text-gray-700 hover:text-gray-900 font-semibold block
-                  pb-2 text-sm"
-                  href="https://creative-tim.com/privacy">
-                  Privacy Policy
-                </a>
-              </li>
-              <li>
-                <a
-                  class="text-gray-700 hover:text-gray-900 font-semibold block
-                  pb-2 text-sm"
-                  href="https://creative-tim.com/contact-us">
-                  Contact Us
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
     <hr class="my-6 border-gray-400" />
     <div class="flex flex-wrap items-center md:justify-between justify-center">
       <div class="w-full md:w-4/12 px-4 mx-auto text-center">
         <div class="text-sm text-gray-600 font-semibold py-1">
-          Copyright © 2019 Tailwind Starter Kit by
-          <a
-            href="https://www.creative-tim.com"
-            class="text-gray-600 hover:text-gray-900">
-            Creative Tim
-          </a>
-          .
+          Copyright © 2021 CHS '73 Organizing Committee
         </div>
       </div>
     </div>
