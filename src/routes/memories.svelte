5eee8d31b1066f32932b900a92d763484731283a
jdmedlock <jdmedlock@gmail.com>
2021-04-07 17:35:21 -0500

    Feature: Adjust responsive layouts

--- src/routes/memories.svelte
+++ src/routes/memories.svelte
@@ -22,34 +22,36 @@
         points="2560 0 2560 100 0 100" />
     </svg>
   </div>
-  <div class="container mx-auto lg:py-none px-4">
-    <div class="flex flex-wrap">
-      <div class="flex justify-center flex-wrap w-full md:w-7/12 m-auto">
-        <img
-          alt="Stay tuned"
-          class="max-w-full h-48 rounded-lg shadow-lg mb-8 lg:mb-none transform scale-x-250"
-          src="chs_lorimer_cemetary.jpeg" />
-        <div class="md:pr-12 text-center">
-          <h3 class="text-3xl font-semibold">In Memoriam</h3>
-          <p class="mt-4 text-lg leading-normal text-gray-600">
+  <div class="container w-full mx-auto lg:py-none px-4">
+    <h1 class="my-12 text-3xl md:text-5xl font-semibold text-center">In Memoriam</h1>
+    <div class="flex flex-wrap justify-center">
+      <div class="flex flex-wrap w-full md:w-6/12 mb-4">
+        <div class="flex w-full justify-center m-0 p-0">
+          <img
+            alt="Light behind cemetary tree"
+            class="max-w-full h-20 md:h-28 lg:h-48 rounded-lg shadow-lg ml-0 md:ml-6 lg:ml-0 mb-8 lg:mb-none transform scale-x-250"
+            src="chs_lorimer_cemetary.jpeg" />
+        </div>
+        <div class="text-center">
+          <p class="text-lg leading-normal text-gray-600">
             Reading the names on this list as you begin that morning cup overflow
             coffee changes your day. Every name brings back a memory....a class,
             a date, a conversation.
           </p>
           <div class="text-2xl font-extrabold text-gray-400 leading-snug">~</div>
-          <p class="mt-4 text-lg leading-normal text-gray-600">
+          <p class="text-lg leading-normal text-gray-600">
             It was a life long ago, but yesterday. We were perched on the rim
             of the world, ready to jump in and make our mark. Hopefully every
             member of this lit got to experience life's thrills.
           </p>
           <div class="text-2xl font-extrabold text-gray-400 leading-snug">~</div>
-          <p class="mt-4 text-lg leading-normal text-gray-600">
+          <p class="text-lg leading-normal text-gray-600">
             A child, an accomplishment, holding hands at the close of the day.
             ...a pat on the back when they needed it most and friends in the world
             final moments.
           </p>
           <div class="text-2xl font-extrabold text-gray-400 leading-snug">~</div>
-          <p class="mt-4 text-lg leading-normal text-gray-600">
+          <p class="text-lg leading-normal text-gray-600">
             They were all an important part of our lives...our dreams. Hopefully
             those of us that remain will represent those who are gone with
             loving pride and honor these memories that make us smile and laugh.
@@ -57,7 +59,7 @@
           </p>
         </div>
       </div>
-      <div class="flex justify-center w-full md:w-4/12 m-0 p-4 border-gray-300 border-2 shadow-2xl">
+      <div class="flex justify-center w-full md:w-4/12 ml-1 md:ml-12 lg:ml-20 p-4 border-gray-300 border-2 shadow-2xl">
         <ul class="mt-2 text-lg text-gray-600 leading-relaxed">
           <li>Paul Abbott</li>
           <li>Barbara Bahn Lohrman</li>
