e4ac4ef4a6b5e99141cbeb2f6b8759e815408d78
jdmedlock <jdmedlock@gmail.com>
2021-04-07 15:51:25 -0500

    Feature: Add memorials to Memories page

--- src/routes/memories.svelte
+++ src/routes/memories.svelte
@@ -22,21 +22,85 @@
         points="2560 0 2560 100 0 100" />
     </svg>
   </div>
-  <div class="container mx-auto py-8 lg:py-none px-4">
+  <div class="container mx-auto lg:py-none px-4">
     <div class="flex flex-wrap">
-      <div class="flex justify-center w-full md:w-4/12 m-auto px-4">
+      <div class="flex justify-center flex-wrap w-full md:w-7/12 m-auto">
         <img
           alt="Stay tuned"
-          class="max-w-full rounded-lg shadow-lg mb-8 lg:mb-none"
-          src="chs_memories.jpeg" />
-      </div>
-      <div class="w-full md:w-5/12 m-auto px-4">
-        <div class="md:pr-12">
-          <h3 class="text-3xl font-semibold">Memories</h3>
+          class="max-w-full h-56 rounded-lg shadow-lg mb-8 lg:mb-none"
+          src="chs_lorimer_cemetary.jpeg" />
+        <div class="md:pr-12 text-center">
+          <h3 class="text-3xl font-semibold">In Memoriam</h3>
           <p class="mt-4 text-lg leading-relaxed text-gray-600">
-            We have something special planned. Stay tuned...
+            Reading the names on this list as you begin that morning cup overflow
+            coffee changes your day. Every name brings back a memory....a class,
+            a date, a conversation.
+          </p>
+          <div class="text-3xl font-extrabold text-gray-400">~</div>
+          <p class="mt-4 text-lg leading-relaxed text-gray-600">
+            It was a life long ago, but yesterday. We were perched on the rim
+            of the world, ready to jump in and make our mark. Hopefully every
+            member of this lit got to experience life's thrills.
+          </p>
+          <div class="text-3xl font-extrabold text-gray-400">~</div>
+          <p class="mt-4 text-lg leading-relaxed text-gray-600">
+            A child, an accomplishment, holding hands at the close of the day.
+            ...a pat on the back when they needed it most and friends in the world
+            final moments.
+          </p>
+          <div class="text-3xl font-extrabold text-gray-400">~</div>
+          <p class="mt-4 text-lg leading-relaxed text-gray-600">
+            They were all an important part of our lives...our dreams. Hopefully
+            those of us that remain will represent those who are gone with
+            loving pride and honor these memories that make us smile and laugh.
+            and remember that time....
           </p>
         </div>
+      </div>
+      <div class="flex justify-center w-full md:w-4/12 m-auto px-4 border-gray-300 border-2 shadow-2xl">
+        <ul class="mt-2 text-lg leading-relaxed text-gray-600 leading-loose">
+          <li>Paul Abbott</li>
+          <li>Barbara Bahn Lohrman</li>
+          <li>John Bertrand</li>
+          <li>Carol Bess</li>
+          <li>Brad Bock</li>
+          <li>Jennifer Cochran</li>
+          <li>Wilfred "Willie" Colon</li>
+          <li>David Davis</li>
+          <li>Marcia England</li>
+          <li>Eddie Eudy</li>
+          <li>Kip Fee</li>
+          <li>Robert Fehr</li>
+          <li>Scott Fischer</li>
+          <li>Terry Gockel</li>
+          <li>Gene Hedden</li>
+          <li>Walter Heimbaugh</li>
+          <li>Eddie Kaiser</li>
+          <li>Kim Kelly Davis</li>
+          <li>Debra Klinkhardt</li>
+          <li>Carl Long</li>
+        </ul>
+        <ul class="ml-6 mt-2 text-lg leading-relaxed text-gray-600 leading-loose">
+          <li>Jennifer Love</li>
+          <li>David Lyons</li>
+          <li>Larry McMackins</li>
+          <li>Randy Mehrle</li>
+          <li>Steve Niswonger</li>
+          <li>Jamie Pelts</li>
+          <li>Deborah Pender</li>
+          <li>Harold Primm</li>
+          <li>Mark Rexroad</li>
+          <li>Robert Schearf</li>
+          <li>Terry Siebert</li>
+          <li>Pam Sitze</li>
+          <li>Mattie Smith</li>
+          <li>Ethel Smith</li>
+          <li>Mark Stoval</li>
+          <li>Mary Volkerding Porter</li>
+          <li>Mark Waldrup</li>
+          <li>Jeff Woods</li>
+          <li>William Mason</li>
+        </ul>
       </div>
     </div>
   </div>
