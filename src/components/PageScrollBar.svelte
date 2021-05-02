2015f062530c0e552c72b156978c61736561dad2
jdmedlock <jdmedlock@gmail.com>
2021-05-02 11:05:04 -0500

    Feature: Add page scroll bar to <Nav> component

---
+++ src/components/PageScrollBar.svelte
@@ -0,0 +1,15 @@
+<script>
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+</script>
+
+<svelte:window on:scroll={ myFunction } />
+
+<div class="w-full h-1 bg-gray-900">
+  <div class="w-full h-1 bg-orange-400" id="myBar"></div>
+</div>
