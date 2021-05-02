42e3343fb81101f49d4d70795d68b067a93fdd37
jdmedlock <jdmedlock@gmail.com>
2021-05-02 11:19:00 -0500

    Feature: Adjust page scroll bar

--- src/components/PageScrollBar.svelte
+++ src/components/PageScrollBar.svelte
@@ -1,14 +1,13 @@
 <script>
-
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("myBar").style.width = scrolled + "%";
-}
+  const fillProgressBar = () => {
+    const winScroll = document.body.scrollTop || document.documentElement.scrollTop
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+    const scrolled = (winScroll / height) * 100
+    document.getElementById("myBar").style.width = scrolled + "%"
+  }
 </script>
 
-<svelte:window on:scroll={ myFunction } />
+<svelte:window on:scroll={ fillProgressBar } />
 
 <div class="w-full h-1 bg-gray-900">
   <div class="w-full h-1 bg-orange-400" id="myBar"></div>
