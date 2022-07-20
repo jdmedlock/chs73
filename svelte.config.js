9ddebb78193ec46c00a5522690b4a6f9940228d4
Jim Medlock <jdmedlock@gmail.com>
2022-07-20 16:54:46 -0500

    Refactor: Prepare for Netlify deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,11 @@
+import adapter from '@sveltejs/adapter-netlify'
 import preprocess from 'svelte-preprocess'
 
 const config = {
-  preprocess: preprocess({ postcss: true })
+  preprocess: preprocess({ postcss: true }),
+  kit: {
+    adapter: adapter()
+  }
 }
 
 export default config
