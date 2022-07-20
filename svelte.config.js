d8995cd57debecfb79f507f17fe56faa4d3780af
Jim Medlock <jdmedlock@gmail.com>
2022-07-20 17:06:16 -0500

    Refactor: Add svelte.config.js file

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,16 @@ import preprocess from 'svelte-preprocess'
 const config = {
   preprocess: preprocess({ postcss: true }),
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      // if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: false,
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false
+    })
   }
 }
 
