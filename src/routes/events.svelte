3dc8b170a074d29b16cad58e7b2c5b435ce4c65f
Jim Medlock <jdmedlock@gmail.com>
2022-07-22 15:30:10 -0500

    Fix: Correct Events page image URL's & scroll obscuring top navbar

--- src/routes/events.svelte
+++ src/routes/events.svelte
@@ -7,159 +7,159 @@
 <section class="relative py-12 lg:py-20">
   
 <div class="bg-gray-900">
-    <div class="pt-12 sm:pt-16 lg:pt-24">
-      <div class="max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8">
-        <div class="max-w-3xl mx-auto space-y-2 lg:max-w-none">
-          <h2 class="text-lg leading-6 font-semibold text-gray-300 uppercase tracking-wider">Upcoming Events</h2>
-          <p class="text-3xl font-extrabold text-white sm:text-4xl lg:text-5xl">Reconnect with classmates at one or both of these two events!</p>
-          <p class="text-xl text-gray-300"></p>
-        </div>
+  <div class="pt-12 sm:pt-16 lg:pt-24">
+    <div class="max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8">
+      <div class="max-w-3xl mx-auto space-y-2 lg:max-w-none">
+        <h2 class="text-lg leading-6 font-semibold text-gray-300 uppercase tracking-wider">Upcoming Events</h2>
+        <p class="text-3xl font-extrabold text-white sm:text-4xl lg:text-5xl">Reconnect with classmates at one or both of these two events!</p>
+        <p class="text-xl text-gray-300"></p>
       </div>
     </div>
-    <div class="mt-8 pb-12 bg-gray-50 sm:mt-12 sm:pb-16 lg:mt-16 lg:pb-24">
-      <div class="relative">
-        <div class="absolute inset-0 h-3/4 bg-gray-900"></div>
-        <div class="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="max-w-md mx-auto space-y-4 lg:max-w-5xl lg:grid lg:grid-cols-2 lg:gap-5 lg:space-y-0">
-            <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-              <div class="px-6 py-8 bg-white sm:p-10 sm:pb-6">
-                <div>
-                  <h3 class="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-indigo-100 text-indigo-600" id="tier-standard">September 15, 2023</h3>
-                </div>
-                <div class="mt-4 flex items-baseline text-6xl font-extrabold">
-                  Dogwood Social House
-                </div>
-                <p class="mt-5 text-xl text-gray-500">An informal gathering to relax & reconnect!</p>
-                <div>
-                  <img class="w-96 h-96" src="/src/assets/dogwood_social_club.png" alt="Dogwood photo"/>
-                </div>
+  </div>
+  <div class="mt-8 pb-12 bg-gray-50 sm:mt-12 sm:pb-16 lg:mt-16 lg:pb-24">
+    <div class="relative">
+      <div class="absolute inset-0 h-3/4 bg-gray-900"></div>
+      <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="max-w-md mx-auto space-y-4 lg:max-w-5xl lg:grid lg:grid-cols-2 lg:gap-5 lg:space-y-0">
+          <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+            <div class="px-6 py-8 bg-white sm:p-10 sm:pb-6">
+              <div>
+                <h3 class="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-indigo-100 text-indigo-600" id="tier-standard">September 15, 2023</h3>
               </div>
-              <div class="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-gray-50 space-y-6 sm:p-10 sm:pt-6">
-                <ul role="list" class="space-y-4">
-                  <li class="flex items-start">
-                    <div class="flex-shrink-0">
-                      <!-- Heroicon name: outline/check -->
-                      <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                      </svg>
-                    </div>
-                    <p class="ml-3 text-base text-gray-700">5:00PM - Closing</p>
-                  </li>
-  
-                  <li class="flex items-start">
-                    <div class="flex-shrink-0">
-                      <!-- Heroicon name: outline/check -->
-                      <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                      </svg>
-                    </div>
-                    <p class="ml-3 text-base text-gray-700">Cash bar & food</p>
-                  </li>
-  
-                  <li class="flex items-start">
-                    <div class="flex-shrink-0">
-                      <!-- Heroicon name: outline/check -->
-                      <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                      </svg>
-                    </div>
-                    <p class="ml-3 text-base text-gray-700">80 S Plaza Way, Cape Girardeau, MO 63703. Phone: (573) 837-1260</p>
-                  </li>
-  
-                  <li class="flex items-start">
-                    <div class="flex-shrink-0">
-                      <!-- Heroicon name: outline/check -->
-                      <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                      </svg>
-                    </div>
-                    <p class="ml-3 text-base text-gray-700"><a href="https://dogwood-social-house.business.site/" target="_blank" rel="noopener noreferrer">Click for Website</a></p>
-                  </li>
-                </ul>
-                <div class="rounded-md shadow">
-                  <a href="#" class="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-900" aria-describedby="tier-standard"> Sign up coming soon! </a>
-                </div>
+              <div class="mt-4 flex items-baseline text-6xl font-extrabold">
+                Dogwood Social House
+              </div>
+              <p class="mt-5 text-xl text-gray-500">An informal gathering to relax & reconnect!</p>
+              <div>
+                <img class="w-96 h-96" src="/dogwood_social_club.png" alt="Dogwood photo"/>
               </div>
             </div>
-  
-            <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-              <div class="px-6 py-8 bg-white sm:p-10 sm:pb-6">
-                <div>
-                  <h3 class="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-indigo-100 text-indigo-600" id="tier-standard">September 16, 2023</h3>
-                </div>
-                <div class="mt-4 flex items-baseline text-6xl font-extrabold">
-                  Drury 
-                </div>
-                <div class="flex items-baseline text-6xl font-extrabold">
-                  Plaza 
-                </div>
-                <p class="mt-5 text-xl text-gray-500">Dinner, music, & sharing of fond memories!</p>
-                <div>
-                  <img class="w-96 h-96" src="/src/assets/drury_plaza.jpeg" alt="Dogwood photo"/>
-                </div>
-              </div>
-              <div class="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-gray-50 space-y-6 sm:p-10 sm:pt-6">
-                <ul role="list" class="space-y-4">
-                  <li class="flex items-start">
-                    <div class="flex-shrink-0">
-                      <!-- Heroicon name: outline/check -->
-                      <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                      </svg>
-                    </div>
-                    <p class="ml-3 text-base text-gray-700">4:00 PM - 11:30 PM</p>
-                  </li>
-  
-                  <li class="flex items-start">
-                    <div class="flex-shrink-0">
-                      <!-- Heroicon name: outline/check -->
-                      <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                      </svg>
-                    </div>
-                    <p class="ml-3 text-base text-gray-700">Dinner + cash bar</p>
-                  </li>
-  
-                  <li class="flex items-start">
-                    <div class="flex-shrink-0">
-                      <!-- Heroicon name: outline/check -->
-                      <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                      </svg>
-                    </div>
-                    <p class="ml-3 text-base text-gray-700">Discounted room rate of $148/night for Sept. 15 & 16 (includes free breakfast + evening drinks & snacks)</p>
-                  </li>
-  
-                  <li class="flex items-start">
-                    <div class="flex-shrink-0">
-                      <!-- Heroicon name: outline/check -->
-                      <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                      </svg>
-                    </div>
-                    <p class="ml-3 text-base text-gray-700">Call 1-800-325-0720 before August 28, 2023 and use Group number 10018741 to make your reservation</p>
-                  </li>
+            <div class="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-gray-50 space-y-6 sm:p-10 sm:pt-6">
+              <ul role="list" class="space-y-4">
+                <li class="flex items-start">
+                  <div class="flex-shrink-0">
+                    <!-- Heroicon name: outline/check -->
+                    <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                    </svg>
+                  </div>
+                  <p class="ml-3 text-base text-gray-700">5:00PM - Closing</p>
+                </li>
 
-                  <li class="flex items-start">
-                    <div class="flex-shrink-0">
-                      <!-- Heroicon name: outline/check -->
-                      <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                      </svg>
-                    </div>
-                    <p class="ml-3 text-base text-gray-700"><a href="https://www.druryhotels.com/locations/cape-girardeau-mo/drury-plaza-hotel-cape-girardeau-conference-center" target="_blank" rel="noopener noreferrer">Click for Website</a></p>
-                  </li>
-                </ul>
-                <div class="rounded-md shadow">
-                  <a href="#" class="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-900" aria-describedby="tier-standard"> Sign up coming soon! </a>
-                </div>
+                <li class="flex items-start">
+                  <div class="flex-shrink-0">
+                    <!-- Heroicon name: outline/check -->
+                    <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                    </svg>
+                  </div>
+                  <p class="ml-3 text-base text-gray-700">Cash bar & food</p>
+                </li>
+
+                <li class="flex items-start">
+                  <div class="flex-shrink-0">
+                    <!-- Heroicon name: outline/check -->
+                    <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                    </svg>
+                  </div>
+                  <p class="ml-3 text-base text-gray-700">80 S Plaza Way, Cape Girardeau, MO 63703. Phone: (573) 837-1260</p>
+                </li>
+
+                <li class="flex items-start">
+                  <div class="flex-shrink-0">
+                    <!-- Heroicon name: outline/check -->
+                    <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                    </svg>
+                  </div>
+                  <p class="ml-3 text-base text-gray-700"><a href="https://dogwood-social-house.business.site/" target="_blank" rel="noopener noreferrer">Click for Website</a></p>
+                </li>
+              </ul>
+              <div class="rounded-md shadow">
+                <a href="#" class="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-900" aria-describedby="tier-standard"> Sign up coming soon! </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+            <div class="px-6 py-8 bg-white sm:p-10 sm:pb-6">
+              <div>
+                <h3 class="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-indigo-100 text-indigo-600" id="tier-standard">September 16, 2023</h3>
+              </div>
+              <div class="mt-4 flex items-baseline text-6xl font-extrabold">
+                Drury 
+              </div>
+              <div class="flex items-baseline text-6xl font-extrabold">
+                Plaza 
+              </div>
+              <p class="mt-5 text-xl text-gray-500">Dinner, music, & sharing of fond memories!</p>
+              <div>
+                <img class="w-96 h-96" src="/drury_plaza.jpeg" alt="Dogwood photo"/>
+              </div>
+            </div>
+            <div class="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-gray-50 space-y-6 sm:p-10 sm:pt-6">
+              <ul role="list" class="space-y-4">
+                <li class="flex items-start">
+                  <div class="flex-shrink-0">
+                    <!-- Heroicon name: outline/check -->
+                    <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                    </svg>
+                  </div>
+                  <p class="ml-3 text-base text-gray-700">4:00 PM - 11:30 PM</p>
+                </li>
+
+                <li class="flex items-start">
+                  <div class="flex-shrink-0">
+                    <!-- Heroicon name: outline/check -->
+                    <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                    </svg>
+                  </div>
+                  <p class="ml-3 text-base text-gray-700">Dinner + cash bar</p>
+                </li>
+
+                <li class="flex items-start">
+                  <div class="flex-shrink-0">
+                    <!-- Heroicon name: outline/check -->
+                    <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                    </svg>
+                  </div>
+                  <p class="ml-3 text-base text-gray-700">Discounted room rate of $148/night for Sept. 15 & 16 (includes free breakfast + evening drinks & snacks)</p>
+                </li>
+
+                <li class="flex items-start">
+                  <div class="flex-shrink-0">
+                    <!-- Heroicon name: outline/check -->
+                    <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                    </svg>
+                  </div>
+                  <p class="ml-3 text-base text-gray-700">Call 1-800-325-0720 before August 28, 2023 and use Group number 10018741 to make your reservation</p>
+                </li>
+
+                <li class="flex items-start">
+                  <div class="flex-shrink-0">
+                    <!-- Heroicon name: outline/check -->
+                    <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                    </svg>
+                  </div>
+                  <p class="ml-3 text-base text-gray-700"><a href="https://www.druryhotels.com/locations/cape-girardeau-mo/drury-plaza-hotel-cape-girardeau-conference-center" target="_blank" rel="noopener noreferrer">Click for Website</a></p>
+                </li>
+              </ul>
+              <div class="rounded-md shadow">
+                <a href="#" class="flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-900" aria-describedby="tier-standard"> Sign up coming soon! </a>
               </div>
             </div>
           </div>
         </div>
       </div>
-
     </div>
+
   </div>
+</div>
 
 </section>
