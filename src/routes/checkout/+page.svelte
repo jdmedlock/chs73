16a6a41c8ae37ffdd04eef1181bc7989089c37e1
jdmedlock <jdmedlock@gmail.com>
2022-10-26 16:51:03 -0500

    Refactor: Remove direct Airtable interface

--- src/routes/checkout/+page.svelte
+++ src/routes/checkout/+page.svelte
@@ -6,6 +6,7 @@
 
   export let cartTotal = 25.00
 
+  let isPaymentVisible = false
   let back = $page.data.params.get('back') || ''
   let backPage = back === "checkout" ? "events" : back
 
@@ -25,41 +26,44 @@
   }
 
   const handleSaturdaySignup = (event) => {
-    loadScript({ "client-id": `${ import.meta.env.VITE_PAYPAL_CLIENT_ID }` }).then((paypal) => {
-    paypal
-      .Buttons({
-        style: {
-          color: "blue",
-          shape: "rect",
-          label: "paypal",
-          layout: "vertical"
-        },
-        createOrder: function (data, actions) {
-          // Set up the transaction
-          return actions.order.create({
-            purchase_units: [
-              {
-                amount: {
-                  value: cartTotal,
+    isPaymentVisible = !isPaymentVisible
+    if (isPaymentVisible) {
+      loadScript({ "client-id": `${ import.meta.env.VITE_PAYPAL_CLIENT_ID }` }).then((paypal) => {
+      paypal
+        .Buttons({
+          style: {
+            color: "blue",
+            shape: "rect",
+            label: "paypal",
+            layout: "vertical"
+          },
+          createOrder: function (data, actions) {
+            // Set up the transaction
+            return actions.order.create({
+              purchase_units: [
+                {
+                  amount: {
+                    value: cartTotal,
+                  },
                 },
-              },
-            ],
-          })
-        },
-        onApprove: function (data, actions) {
-          // Capture order after payment approved
-          return actions.order.capture().then(function (details) {
-            alert("Payment successful!")
-          });
-        },
-        onError: function (err) {
-          // Log error if something goes wrong during approval
-          alert("Something went wrong");
-          console.log("Something went wrong", err)
-        },
+              ],
+            })
+          },
+          onApprove: function (data, actions) {
+            // Capture order after payment approved
+            return actions.order.capture().then(function (details) {
+              alert("Payment successful!")
+            });
+          },
+          onError: function (err) {
+            // Log error if something goes wrong during approval
+            alert("Something went wrong");
+            console.log("Something went wrong", err)
+          },
+        })
+        .render("#paypal-button-container")
       })
-      .render("#paypal-button-container")
-  })
+    }
   }
 </script>
 
@@ -70,102 +74,84 @@
 </style>
 
 <section class="relative">
-
-<div class="bg-gray-900">
-<div class="pt-12 sm:pt-16 lg:pt-24">
-  <div class="max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8">
-    <div class="max-w-3xl mx-auto space-y-2 lg:max-w-none">
-      <h2 class="text-lg leading-6 font-semibold text-gray-300 uppercase tracking-wider">Signup for Saturday</h2>
-      <p class="text-3xl font-extrabold text-white sm:text-4xl lg:text-5xl">Signup & pay for Saturday's Classmate Gathering</p>
-      <p class="text-xl text-gray-300"></p>
-    </div>
-  </div>
-</div>
-<div class="mt-8 pb-12 bg-gray-50 sm:mt-12 sm:pb-16 lg:mt-16 lg:pb-24">
-  <div class="relative">
-    <div class="absolute inset-0 h-3/4 bg-gray-900"></div>
-    <div class="relative mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="max-w-md mx-auto space-y-4 lg:max-w-5xl lg:grid lg:grid-cols-1 lg:gap-5 lg:space-y-0">
-        <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-          <div class="px-6 py-8 bg-white sm:p-10 sm:pb-6">
-            <div>
-              <h3 class="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-indigo-100 text-indigo-600" id="tier-standard">September 16, 2023</h3>
-            </div>
-            <div class="mt-4 flex items-baseline text-6xl font-extrabold">
-              Drury Plaza
-            </div>
-            <div class="flex flex-wrap w-full m-0 p-0 justify-center">
-              <p class="mt-5 text-xl text-gray-500">Hors d’oeurves, cash bar, & a time to remember!</p>
-            </div>
-          </div>
-          <div class="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-gray-50 space-y-6 sm:p-10 sm:pt-6">
-            <ul class="space-y-4">
-              <li class="flex items-start">
-                <div class="flex-shrink-0">
-                  <!-- Heroicon name: outline/check -->
-                  <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                  </svg>
-                </div>
-                <p class="ml-3 text-base text-gray-700">4:00 PM - 11:30 PM</p>
-              </li>
-
-              <li class="flex items-start">
-                <div class="flex-shrink-0">
-                  <!-- Heroicon name: outline/check -->
-                  <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                  </svg>
-                </div>
-                <p class="ml-3 text-base text-gray-700">Hors d’oeurves + cash bar</p>
-              </li>
-
-              <li class="flex items-start">
-                <div class="flex-shrink-0">
-                  <!-- Heroicon name: outline/check -->
-                  <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                  </svg>
-                </div>
-                <p class="ml-3 text-base text-gray-700">Discounted room rate of $148/night for Sept. 15 & 16 (includes free breakfast + evening drinks & snacks)</p>
-              </li>
-
-              <li class="flex items-start">
-                <div class="flex-shrink-0">
-                  <!-- Heroicon name: outline/check -->
-                  <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                  </svg>
-                </div>
-                <p class="ml-3 text-base text-gray-700">Call 1-800-325-0720 before August 28, 2023 and use Group number 10018741 to make your reservation</p>
-              </li>
-
-              <li class="flex items-start">
-                <div class="flex-shrink-0">
-                  <!-- Heroicon name: outline/check -->
-                  <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
-                  </svg>
-                </div>
-                <p class="ml-3 text-base text-gray-700"><a href="https://www.druryhotels.com/locations/cape-girardeau-mo/drury-plaza-hotel-cape-girardeau-conference-center" target="_blank" rel="noopener noreferrer">Click for Website</a></p>
-              </li>
-            </ul>
-            <button class="atcb flex items-center m-auto" on:click={ handleAddDruryToCalendar }>
-              <span class="inline-flex items-center px-3 py-0.5 rounded-full text-2xl font-medium bg-orange-500 text-white"> Save this to my calendar!!! </span>
-            </button>
-            <button class="flex items-center m-auto" on:click={ handleSaturdaySignup }>
-              <span class="inline-flex items-center px-3 py-0.5 rounded-full text-2xl font-medium bg-orange-500 text-white"> Signup for Saturdays gathering!!! </span>
-            </button>
-          </div>
-
-          <div id="paypal-button-container" />
-
+  <div class="bg-gray-900">
+    <div class="pt-12 sm:pt-16 lg:pt-24">
+      <div class="max-w-7xl mx-auto text-center px-4 sm:px-6 lg:px-8">
+        <div class="max-w-3xl mx-auto space-y-2 lg:max-w-none">
+          <h2 class="text-lg leading-6 font-semibold text-gray-300 uppercase tracking-wider">Signup for Saturday</h2>
+          <p class="text-3xl font-extrabold text-white sm:text-4xl lg:text-5xl">Signup & pay for Saturday's Classmate Gathering</p>
+          <p class="text-xl text-gray-300"></p>
         </div>
       </div>
     </div>
+    <div class="mt-8 pb-12 bg-gray-50 sm:mt-12 sm:pb-16 lg:mt-16 lg:pb-24">
+      <div class="relative">
+        <div class="absolute inset-0 h-3/4 bg-gray-900"></div>
+        <div class="relative mx-auto px-4 sm:px-6 lg:px-8">
+          <div class="max-w-md mx-auto space-y-4 lg:max-w-5xl lg:grid lg:grid-cols-1 lg:gap-5 lg:space-y-0">
+            <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+              <div class="px-6 py-8 bg-gray-50 sm:p-10 sm:pb-6">
+                <div>
+                  <h3 class="inline-flex px-4 py-1 rounded-full text-sm font-semibold tracking-wide uppercase bg-indigo-100 text-indigo-600" id="tier-standard">September 16, 2023</h3>
+                </div>
+                <div class="mt-4 flex items-baseline text-6xl font-extrabold">
+                  Drury Plaza
+                </div>
+                <div class="flex flex-wrap w-full m-0 p-0 justify-center">
+                  <p class="mt-5 text-xl text-gray-500">Hors d’oeurves, cash bar, & a time to remember!</p>
+                </div>
+              </div>
+              <div class="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-gray-50 space-y-6 sm:p-10 sm:pt-6">
+                <ul class="space-y-4">
+                  <li class="flex items-start">
+                    <div class="flex-shrink-0">
+                      <!-- Heroicon name: outline/check -->
+                      <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                      </svg>
+                    </div>
+                    <p class="ml-3 text-base text-gray-700">4:00 PM - 11:30 PM</p>
+                  </li>
+
+                  <li class="flex items-start">
+                    <div class="flex-shrink-0">
+                      <!-- Heroicon name: outline/check -->
+                      <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                      </svg>
+                    </div>
+                    <p class="ml-3 text-base text-gray-700">Hors d’oeurves + cash bar</p>
+                  </li>
+
+                                  <li class="flex items-start">
+                    <div class="flex-shrink-0">
+                      <!-- Heroicon name: outline/check -->
+                      <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                      </svg>
+                    </div>
+                    <p class="ml-3 text-base text-gray-700">$25 for the evening</p>
+                  </li>
+                </ul>
+                <button class="atcb flex items-center m-auto" on:click={ handleAddDruryToCalendar }>
+                  <span class="inline-flex items-center px-3 py-0.5 rounded-full text-2xl font-medium bg-orange-500 text-white"> Save this to my calendar!!! </span>
+                </button>
+                <button class="flex items-center m-auto" on:click={ handleSaturdaySignup }>
+                  <span class="inline-flex items-center px-3 py-0.5 rounded-full text-2xl font-medium bg-orange-500 text-white"> Signup for Saturdays gathering!!! </span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    {#if isPaymentVisible}
+      <div class="flex flex-col items-center rounded-lg shadow-lg bg-gray-200">
+        <div id="paypal-button-container" />
+      </div>
+    {/if}
+
   </div>
-
-</div>
-</div>
-
 </section>
