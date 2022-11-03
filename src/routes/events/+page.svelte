fa7a95b8958236c22cc96564a1501196437d9a2c
jdmedlock <jdmedlock@gmail.com>
2022-11-03 16:26:24 -0500

    Feature: Indent hotel info on Events page

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -151,7 +151,7 @@
                   <p class="ml-3 text-base text-gray-700">Discounted room rate of $148/night for Sept. 15 & 16 (includes free breakfast + evening drinks & snacks)</p>
                 </li>
 
-                <li class="flex items-start">
+                <li class="ml-8 flex items-start">
                   <div class="flex-shrink-0">
                     <!-- Heroicon name: outline/check -->
                     <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
@@ -161,7 +161,7 @@
                   <p class="ml-3 text-base text-gray-700">Call 1-800-325-0720 before August 28, 2023 and use Group number 10018741 to make your reservation</p>
                 </li>
 
-                <li class="flex items-start">
+                <li class="ml-8 flex items-start">
                   <div class="flex-shrink-0">
                     <!-- Heroicon name: outline/check -->
                     <svg class="h-6 w-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
