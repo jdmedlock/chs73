429c80bd3c8f6d2a481b3b7c6db8e070409d30da
jdmedlock <jdmedlock@gmail.com>
2022-12-17 07:34:00 -0600

    Refactor: Remove commented out code

--- src/routes/checkout/+page.svelte
+++ src/routes/checkout/+page.svelte
@@ -507,31 +507,6 @@
         classmateFirstName={ classmateFirstName } classmateLastName={ classmateLastName }
         companionFirstName={ companionFirstName } companionLastName={ companionLastName }
       />
-      <!--
-      <div class="flex flex-col items-center bg-white text-base">
-        <h3 id="paymentReceipt" class="mt-2 rounded-full font-semibold tracking-wide uppercase bg-indigo-100 text-indigo-600">Your payment was successfully processed (check your email for a reciept)</h3>
-        <div class="grid grid-cols-2 gap-x-4 mt-4 ml-8 bg-gray-200 w-1/2 mb-20">
-          <div>Order ID:</div><div id="receiptOrderId">{ resultDetails.id }</div>
-          <div>Amount:</div><div id="receiptAmount">{ resultDetails.purchase_units[0].amount.value }</div>
-          <div>Transaction status:</div><div id="receiptStatus">{ resultDetails.status }</div>
-          <div>Transaction created:</div><div id="receiptCreated">{ resultDetails.create_time }</div>
-          <div>Name:</div><div id="receiptName"> { resultDetails.payer.name.given_name } { resultDetails.payer.name.surname }</div>
-          <div>Address:</div><div id="receiptAddress">{ resultDetails.purchase_units[0].shipping.address.address_line_1 }</div>
-          {#if resultDetails.purchase_units[0].shipping.address.address_line_2}
-            <div>&nbsp;</div><div id="receiptAddress2">{ resultDetails.purchase_units[0].shipping.address.address_line_2 }</div>
-          {/if}
-          <div>City:</div><div id="receiptCity">{ resultDetails.purchase_units[0].shipping.address.admin_area_2 }</div>
-          <div>State:</div><div id="receiptState">{ resultDetails.purchase_units[0].shipping.address.admin_area_1 }</div>
-          <div>Zipcode:</div><div id="receiptPostalCode">{ resultDetails.purchase_units[0].shipping.address.postal_code }</div>
-          <div>Email:</div><div id="receiptEmail">{ resultDetails.payer.email_address }</div>
-          <div>Agreed to isSponsor another classmate:</div><div id="receiptSponsor">{ isSponsor ? 'Yes' : 'No' }</div>
-          <div>Your badge name:</div><div id="receiptClassmateBadge">{ classmateFirstName } { classmateLastName }</div>
-          {#if noAttendees > 1}
-            <div>Companion badge name:</div><div id="receiptCompanionBadge">{ companionFirstName } { companionLastName }</div>
-          {/if}
-        </div>
-      </div>
-      -->
     {/if}
 
   </div>
