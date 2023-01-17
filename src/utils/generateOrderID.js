d9657e3825133d6c35cc0369b10707c53c079707
jdmedlock <jdmedlock@gmail.com>
2023-01-17 09:49:52 -0600

    Fix: Make generated order id unique

--- src/utils/generateOrderID.js
+++ src/utils/generateOrderID.js
@@ -1,8 +1,9 @@
 // Convert a string into a hash code that will be used as a unique order ID
 const generateOrderID = (concatenatedName) => {
+  const originalName = concatenatedName.concat(Date.now()) // Make it unique
   let orderID = 0
-  for (let i = 0; i < concatenatedName.length; i++) {
-      const code = concatenatedName.charCodeAt(i)
+  for (let i = 0; i < originalName.length; i++) {
+      const code = originalName.charCodeAt(i)
       orderID = ((orderID<<5) - orderID) + code
       orderID = Math.abs(orderID & orderID) // Convert to a positive 32bit integer
   }
