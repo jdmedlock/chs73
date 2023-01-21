f0aff7804a84b0deb959280f925e10c6cd81ba6d
Jim Medlock <jdmedlock@gmail.com>
2023-01-21 17:01:57 -0600

    Feature: Finish styling of mail to payment address instructions

--- src/routes/checkout/+page.svelte
+++ src/routes/checkout/+page.svelte
@@ -482,11 +482,20 @@
           Pay by mail 
         </button>
         {#if isPayByMail}
-          <div>Please send your registration fee to:</div>
-          <div>CHS '73 Reunion Treasurer</div>
-          <div>1234 Dale St.</div>
-          <div>Cape Girardeau, MO  63301</div>
-          <div>Please add your order number, '1234', to your checks memo.</div>
+          <div class="w-1/2 mt-4 ml-8 bg-gray-200">
+            <div class="flex justify-center content-center">
+              <span>Please make your check out to</span>
+              <span class="text-lg font-bold italic ml-2 mr-2 pl-1 pr-1 bg-orange-400">Central High Class of 1973</span>
+              <span>and mail to:</span>
+            </div>
+            <div class="grid grid-cols-3 mt-2 justify-left">
+              <div /><div>Central High Class of 1973</div><div />
+              <div /><div>C/o Dianna Todt</div><div />
+              <div /><div>2086 Southern Expressway</div><div />
+              <div /><div>Cape Girardeau, MO  63703</div><div />
+            </div>
+            <div class="flex mt-2 justify-center">Please add your order number, '1234', to your checks memo.</div>
+          </div>
         {/if}
         <button class="flex items-center justify-center m-auto w-[200px] mt-3 text-base italic font-bold text-white bg-blue-700 h-9 rounded-[4px]" on:click={ handlePayAtDoor }>
           Pay at door 
