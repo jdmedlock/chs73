b5ffb255941a89cdb5351bc61a8f2193a13ed141
Jim Medlock <jdmedlock@gmail.com>
2023-02-22 14:46:09 -0600

    Feature: Add new reunion events [WIP]

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -13,16 +13,19 @@
     <div class="px-4 mx-auto text-center max-w-7xl sm:px-6 lg:px-8">
       <div class="max-w-3xl mx-auto space-y-2 lg:max-w-none">
         <h2 class="text-lg font-semibold leading-6 tracking-wider text-gray-300 uppercase">Upcoming Events</h2>
-        <p class="text-3xl font-extrabold text-white sm:text-4xl lg:text-5xl">Reconnect with classmates at one or both of these two events!</p>
+        <p class="text-3xl font-extrabold text-white sm:text-4xl lg:text-5xl">Reconnect with classmates at these events!</p>
         <p class="text-xl text-gray-300"></p>
       </div>
     </div>
   </div>
+  
   <div class="pb-12 mt-8 bg-gray-50 sm:mt-12 sm:pb-16 lg:mt-16 lg:pb-24">
     <div class="relative">
-      <div class="absolute inset-0 bg-gray-900 h-3/4"></div>
+      <div class="absolute inset-0 bg-gray-900 h-1/2"></div>
       <div class="relative px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-        <div class="max-w-md mx-auto space-y-4 lg:max-w-5xl lg:grid lg:grid-cols-2 lg:gap-5 lg:space-y-0">
+        <div class="max-w-md mx-auto space-y-4 lg:max-w-5xl lg:grid lg:grid-cols-3 lg:gap-3 lg:space-y-0">
+          
+          <!-- Friday Night Event description -->
           <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
             <div class="px-6 py-8 bg-white sm:p-10 sm:pb-6">
               <div>
@@ -70,6 +73,7 @@
             </div>
           </div>
 
+          <!-- Saturday Night Event description -->
           <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
             <div class="px-6 py-8 bg-white sm:p-10 sm:pb-6">
               <div>
@@ -118,6 +122,59 @@
                 <span class="inline-flex items-center px-3 py-0.5 rounded-full text-2xl font-medium bg-orange-500 text-white">Sign me up!!!</span>
               </a>
             </div>
+          </div>
+        </div>
+        
+        <div class="max-w-md mx-auto space-y-4 lg:max-w-5xl lg:grid lg:grid-cols-3 lg:gap-3 lg:space-y-0">
+
+        <!-- Golfing Event description -->
+        <div class="flex flex-col overflow-hidden rounded-lg shadow-lg">
+          <div class="px-6 py-8 bg-white sm:p-10 sm:pb-6">
+            <div>
+              <h3 class="inline-flex px-4 py-1 text-sm font-semibold tracking-wide text-indigo-600 uppercase bg-indigo-100 rounded-full" id="tier-standard">
+                {saturdayEvent.date}
+              </h3>
+            </div>
+            <div class="flex items-baseline mt-4 text-6xl font-extrabold">
+              { saturdayEvent.events.title[0]}
+            </div>
+            <div class="flex items-baseline text-6xl font-extrabold">
+              { saturdayEvent.events.title[1]} 
+            </div>
+            <div class="flex flex-wrap justify-center w-full p-0 m-0">
+              <p class="mt-5 text-xl text-gray-500">Hors d’oeurves, cash bar, & a time to remember!</p>
+              <img class="w-96 h-96" src="drury_plaza.jpeg" alt="Drury Plaza"/>
+            </div>
+          </div>
+          <div class="flex flex-col justify-between flex-1 px-6 pt-6 pb-8 space-y-6 bg-gray-50 sm:p-10 sm:pt-6">
+            <ul class="space-y-4">
+              {#each saturdayEvent.events.summaryInfo as summaryBullet}
+                <li class="flex items-start">
+                  <div class="flex-shrink-0">
+                    <!-- Heroicon name: outline/check -->
+                    <svg class="w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                    </svg>
+                  </div>
+                  <p class="ml-3 text-base text-gray-700">
+                    { summaryBullet }
+                  </p>
+                </li>
+              {/each}
+
+              <li class="flex items-start">
+                <div class="flex-shrink-0">
+                  <!-- Heroicon name: outline/check -->
+                  <svg class="w-6 h-6 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                  </svg>
+                </div>
+                <p class="ml-3 text-base text-gray-700 underline"><a href="https://www.druryhotels.com/locations/cape-girardeau-mo/drury-plaza-hotel-cape-girardeau-conference-center" target="_blank" rel="noopener noreferrer">Click for Website</a></p>
+              </li>
+            </ul>
+            <a id="SaturdaySignupLink" class="flex items-center m-auto" href="signup?event=saturday">
+              <span class="inline-flex items-center px-3 py-0.5 rounded-full text-2xl font-medium bg-orange-500 text-white">Sign me up!!!</span>
+            </a>
           </div>
         </div>
       </div>
